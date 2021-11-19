1411fa28b6d75674fd158e5175c99ce9e50ce93c
rschpdr <pedro.resch@gmail.com>
2021-11-19 10:26:29 -0300

    criei a rota de produtos

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 
 import HomePage from "./HomePage";
 import About from "./About";
+import Products from "./Products";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/about" element={<About />} />
+        <Route path="/products" element={<Products />} />
       </Routes>
     </div>
   );
